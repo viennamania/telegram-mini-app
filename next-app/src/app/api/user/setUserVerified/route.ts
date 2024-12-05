02b50dbcc848afa15a41f0132346c06166bb9b6f
seunghyonpark <creath.park@gmail.com>
2024-12-05 23:24:54 +0900

    Refactor user verification API to use dynamic endpoint based on center and remove commented code

--- next-app/src/app/api/user/setUserVerified/route.ts
+++ next-app/src/app/api/user/setUserVerified/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse, type NextRequest } from "next/server";
 
+
+/*
 import {
 	insertOneVerified,
 } from '@lib/api/user';
-
+*/
 
 
 export async function POST(request: NextRequest) {
@@ -22,7 +24,7 @@ export async function POST(request: NextRequest) {
 
   
 
-
+  /*
   const result = await insertOneVerified({
     walletAddress: walletAddress,
     nickname: nickname,
@@ -31,7 +33,38 @@ export async function POST(request: NextRequest) {
     telegramId: telegramId,
     center: center,
   });
+  */
 
+  let apiURL = "https://owinwallet.com/api/user/setUserVerified";
+
+  if (center === "ppump_orry_bot") {
+    apiURL = "https://aiagentbot.vercel.app/api/user/setUserVerified";
+  }
+
+
+
+  const response = await fetch(apiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      walletAddress: walletAddress,
+      nickname: nickname,
+      userType: userType,
+      mobile: mobile,
+      telegramId: telegramId,
+      center: center,
+    }),
+  });
+
+  if (!response.ok) {
+    return NextResponse.error();
+  }
+
+  const jsonObj = await response.json();
+
+  const result = jsonObj?.result;
 
  
   return NextResponse.json({
@@ -39,5 +72,6 @@ export async function POST(request: NextRequest) {
     result,
     
   });
+
   
 }
