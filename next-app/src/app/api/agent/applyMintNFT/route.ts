867d0b85ccd6efed780b489fddceebbc70ea83ef
seunghyonpark <creath.park@gmail.com>
2025-01-07 21:11:38 +0900

    Refactor applyMintNFT route to streamline request handling and remove unused code

--- next-app/src/app/api/agent/applyMintNFT/route.ts
+++ next-app/src/app/api/agent/applyMintNFT/route.ts
@@ -1,58 +1,14 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-import {
-	insertOne,
-} from '@lib/api/agent';
-
-// getOneByContractAddress
-import {
-  getOneByContractAddress,
-} from '@lib/api/user';
-
-import twilio from "twilio";
-
 export async function POST(request: NextRequest) {
 
   const body = await request.json();
 
-  /*
-  walletAddress: address,
-  agentBot: agentBot,
-  userName: userName,
-  userPhoneNumber: userPhoneNumber,
-  userEmail: userEmail,
-  htxUid: htxUid,
-  htxUsdtWalletAddress: htxUsdtWalletAddress,
-  apiAccessKey: apiAccessKey,
-  apiSecretKey: apiSecretKey,
-  */
-
-  const { center, walletAddress, agentBot, agentBotNumber, userName, userPhoneNumber, userEmail, userTelegramId, htxUserId, htxUsdtWalletAddress, apiAccessKey, apiSecretKey } = body;
 
 
-  /*
+  const { marketingCenter, center, walletAddress, agentBot, agentBotNumber, userName, userPhoneNumber, userEmail, userTelegramId, exchange, htxUsdtWalletAddress, apiAccessKey, apiSecretKey, apiPassword } = body;
 
-  const result = await insertOne({
-    walletAddress: walletAddress,
-    agentBot: agentBot,
-    agentBotNumber: agentBotNumber,
-    userName: userName,
-    userPhoneNumber: userPhoneNumber,
-    userEmail: userEmail,
-    htxUserId: htxUserId,
-    htxUsdtWalletAddress: htxUsdtWalletAddress,
-    apiAccessKey: apiAccessKey,
-    apiSecretKey: apiSecretKey,
-  });
 
-  if (!result) {
-    return NextResponse.error();
-  }
-
-  //console.log("result", result);
-
-  const applicationId = result.id;
-  */
 
 
   const response = await fetch("https://owinwallet.com/api/agent/applyMintNFT", {
@@ -61,6 +17,7 @@ export async function POST(request: NextRequest) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
+      marketingCenter: marketingCenter,
       center: center,
       walletAddress: walletAddress,
       agentBot: agentBot,
@@ -69,10 +26,12 @@ export async function POST(request: NextRequest) {
       userPhoneNumber: userPhoneNumber,
       userEmail: userEmail,
       userTelegramId: userTelegramId,
-      htxUserId: htxUserId,
+      exchange: exchange,
+ 
       htxUsdtWalletAddress: htxUsdtWalletAddress,
       apiAccessKey: apiAccessKey,
       apiSecretKey: apiSecretKey,
+      apiPassword: apiPassword,
     }),
   });
 
@@ -94,53 +53,6 @@ export async function POST(request: NextRequest) {
   */
 
   const result = jsonObj?.result;
-
-  const applicationId = result.id;
-
-
-
-
-
-  /*
-  // send sms to agent holder
-  // get user phone number by erc721ContractAddress is agentBot
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  const client = twilio(accountSid, authToken);
-
-
-  const user = await getOneByContractAddress(agentBot);
-  if (user) {
-    const { mobile } = user;
-
-    if (mobile && mobile.length > 10) {
-
-      const msgBody = `[PPUMP] [TID:#${applicationId}] You have a new agent application from [${userName}]`;
-
-      const message = await client.messages.create({
-        body: msgBody,
-        from: "+17622254217",
-        to: mobile,
-      });
-
-    }
-
-  }
-
-
-  if (userPhoneNumber && userPhoneNumber.length > 10) {
-
-    // send sms to userPhoneNumber
-    const msgBody = `[PPUMP] [TID:#${applicationId}] Your master bot application has been submitted successfully!`;
-
-    const message = await client.messages.create({
-      body: msgBody,
-      from: "+17622254217",
-      to: userPhoneNumber,
-    });
-
-  }
-  */
 
  
   return NextResponse.json({
