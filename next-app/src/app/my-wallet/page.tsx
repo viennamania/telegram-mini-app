955be3df813dbd29f01b30472f1fc7e89c27bead
seunghyonpark <creath.park@gmail.com>
2025-01-19 10:14:58 +0900

    Remove commented-out test address from ProfilePage

--- next-app/src/app/my-wallet/page.tsx
+++ next-app/src/app/my-wallet/page.tsx
@@ -90,10 +90,10 @@ function ProfilePage() {
 
 
 
-    //const address = account?.address;
+    const address = account?.address;
 
     // test address
-    const address = "0x542197103Ca1398db86026Be0a85bc8DcE83e440";
+    ////const address = "0x542197103Ca1398db86026Be0a85bc8DcE83e440";
   
 
 
