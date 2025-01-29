5220f3a87f7ea7c5f2f203d6320c4ae60b71f720
seunghyonpark <creath.park@gmail.com>
2025-01-29 19:40:52 +0900

    Update background color of transfer sections in ProfilePage component

--- next-app/src/app/my-wallet-noahk/page.tsx
+++ next-app/src/app/my-wallet-noahk/page.tsx
@@ -902,7 +902,7 @@ function ProfilePage() {
                                 {/* send NOAHK 포인트 */}
 
                                 <div className='w-full flex flex-col gap-2 items-start justify-between border border-gray-300 p-4 rounded-lg
-                                    bg-zinc-800'>
+                                    bg-yellow-500 bg-opacity-50'>
                                     <div className="bg-green-500 text-sm text-zinc-100 p-2 rounded">
                                         NOAHK 포인트 보내기
                                     </div>
@@ -1130,7 +1130,7 @@ function ProfilePage() {
 
                     {transfers?.length > 0 && (
                         <div className='w-full flex flex-col gap-2 items-start justify-between border border-gray-300 p-4 rounded-lg
-                            bg-zinc-800'>
+                            bg-yellow-500 bg-opacity-50'>
                             <div className="bg-green-500 text-sm text-zinc-100 p-2 rounded">
                                 거래내역
                             </div>
