2a2c3a008a755b9bd4f6b5d52a47c7e8d512976d
seunghyonpark <creath.park@gmail.com>
2025-02-02 20:04:36 +0900

    Refactor acceptSellOrder route: streamline user data retrieval and clean up commented code

--- next-app/src/app/api/cronjob/acceptSellOrder/route.ts
+++ next-app/src/app/api/cronjob/acceptSellOrder/route.ts
@@ -90,6 +90,27 @@ export async function GET(request: NextRequest) {
     });
   }
 
+
+  const buyerWalletAddress = walletAddress;
+
+  // getOneByWalletAddress
+  const user = await getOneByWalletAddress(buyerWalletAddress);
+
+  if (!user) {
+    return NextResponse.json({
+      result: null,
+    });
+  }
+
+
+  const buyerNickname = user.nickname;
+  const buyerAvatar = user.avatar;
+  const buyerMobile = user.mobile;
+  const buyerMemo = "";
+  const depositName = "";
+  const depositBankName = "";
+
+
   //console.log("sellOrders", sellOrders);
 
   /*
@@ -112,33 +133,14 @@ export async function GET(request: NextRequest) {
   */
 
 
-  //sellOrders.forEach(async (sellOrder: any) => {
 
-  if (sellOrders.length > 0) {
+  sellOrders.forEach(async (sellOrder: any) => {
 
-    const sellOrder = sellOrders[0];
+  //if (sellOrders.length > 0) {
+
+    //const sellOrder = sellOrders[0];
 
     const orderId = sellOrder._id.toString();
-
-
-    const buyerWalletAddress = walletAddress;
-
-    // getOneByWalletAddress
-    const user = await getOneByWalletAddress(buyerWalletAddress);
-
-    if (!user) {
-      return NextResponse.json({
-        result: null,
-      });
-    }
-
-
-    const buyerNickname = user.nickname;
-    const buyerAvatar = user.avatar;
-    const buyerMobile = user.mobile;
-    const buyerMemo = "";
-    const depositName = "";
-    const depositBankName = "";
 
 
     // escrowWalletAddress, escrowWalletPrivateKey
@@ -224,151 +226,9 @@ export async function GET(request: NextRequest) {
 
     }
 
-  }
+
+  } );
   
-
-
-
-
-
-  /*
-  const body = await request.json();
-
-  const { lang, chain, orderId, buyerWalletAddress, buyerNickname, buyerAvatar, buyerMobile, buyerMemo, depositName, depositBankName } = body;
-
-  console.log("orderId", orderId);
-
-
-
-
-
-  const escrowWalletPrivateKey = ethers.Wallet.createRandom().privateKey;
-
-  //console.log("escrowWalletPrivateKey", escrowWalletPrivateKey);
-
-  if (!escrowWalletPrivateKey) {
-    return NextResponse.json({
-      result: null,
-    });
-  }
-
-
-
-  const client = createThirdwebClient({
-    secretKey: process.env.THIRDWEB_SECRET_KEY || "",
-  });
-
-  if (!client) {
-    return NextResponse.json({
-      result: null,
-    });
-  }
-
-
-  const personalAccount = privateKeyToAccount({
-    client,
-    privateKey: escrowWalletPrivateKey,
-  });
-    
-
-  if (!personalAccount) {
-    return NextResponse.json({
-      result: null,
-    });
-  }
-  
-  const wallet = smartWallet({
-    chain: polygon,
-    sponsorGas: true,
-  });
-
-
-  // Connect the smart wallet
-  const account = await wallet.connect({
-    client: client,
-    personalAccount: personalAccount,
-  });
-
-  if (!account) {
-    return NextResponse.json({
-      result: null,
-    });
-  }
-
-
-  const escrowWalletAddress = account.address;
-
-    
-
-
-
-
-  
-
-  const result = await acceptSellOrder({
-    lang: lang,
-    chain: chain,
-    orderId: orderId,
-    buyerWalletAddress: buyerWalletAddress,
-    buyerNickname: buyerNickname,
-    buyerAvatar: buyerAvatar,
-    buyerMobile: buyerMobile,
-    buyerMemo: buyerMemo,
-    depositName: depositName,
-    depositBankName: depositBankName,
-    escrowWalletAddress: escrowWalletAddress,
-    escrowWalletPrivateKey: escrowWalletPrivateKey,
-
-  });
-
-  ///console.log("result", result);
-
-
-  if (!result) {
-    return NextResponse.json({
-      result: null,
-    });
-  }
-
-  // telegram message to seller
-
-  const sellOrder = await getOneSellOrderForEscrow({
-    orderId: orderId,
-  });
-
-  if (sellOrder) {
-    
-    
-
-    const sellerWalletAddress = sellOrder.seller.walletAddress;
-
-    const user = await getOneByWalletAddress(sellerWalletAddress);
-
-    if (user) {
-      
-      const center = user.center;
-      
-      if (sellerWalletAddress) {
-
-        const messagetext = '겨래번호: ' + '#' + sellOrder.tradeId
-         '\n\n판매자가 구매를 수락하였습니다.';
-
-        const result = await insertOtcMessageByWalletAddress({
-          center: center,
-          walletAddress: sellerWalletAddress,
-          sellOrder: sellOrder,
-          message: messagetext,
-        } );
-
-        //console.log("insertOtcMessageByWalletAddress result", JSON.stringify(result));
-
-      }
-
-    }
-
-  }
-
-  */
 
 
 
