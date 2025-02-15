7d9fa538c566aba0e931a001bd19e863d3dabb46
seunghyonpark <creath.park@gmail.com>
2025-02-15 20:45:08 +0900

    Add functionality to place 8 random horses in the race game

--- next-app/src/app/api/game/setRaceGame/route.ts
+++ next-app/src/app/api/game/setRaceGame/route.ts
@@ -13,10 +13,37 @@ export async function POST(request: NextRequest) {
   const { walletAddress } = body;
 
   //console.log("walletAddress", walletAddress);
+
+
+  // place 8 horses
+  // contractAddress = 0x41FBA0bd9f4DC9a968a10aEBb792af6A09969F60
+  // tokenId 0 to 9999
+  // select 8 horses from 0 to 9999
+
+  // 1. get 8 random numbers from 0 to 9999
+  // 2. place 8 horses with the random numbers
+  // non duplicate random numbers
+
+  const horses = [];
+  const randomNumbers = [] as number[];
+  while (randomNumbers.length < 8) {
+    const randomNumber = Math.floor(Math.random() * 10000);
+    if (!randomNumbers.includes(randomNumber)) {
+      randomNumbers.push(randomNumber);
+      horses.push({
+        tokenId: randomNumber,
+      });
+    }
+  }
+
+
+
+
   
 
   const result = await insertOneRaceGame({
     walletAddress: walletAddress,
+    horses: horses,
     usdtAmount: 0,
     krwAmount: 0,
     rate: 0,
