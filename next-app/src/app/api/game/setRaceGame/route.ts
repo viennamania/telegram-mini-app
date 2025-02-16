6bd00e6bf86b49f8f4e74fc2c9e772ca7abe2e49
seunghyonpark <creath.park@gmail.com>
2025-02-16 15:16:00 +0900

    Integrate Thirdweb client for NFT handling in race game and refactor horse selection logic

--- next-app/src/app/api/game/setRaceGame/route.ts
+++ next-app/src/app/api/game/setRaceGame/route.ts
@@ -6,6 +6,32 @@ import {
 
 
 
+
+import {
+  createThirdwebClient,
+  getContract,
+} from "thirdweb";
+
+
+//import { polygonAmoy } from "thirdweb/chains";
+import { polygon } from "thirdweb/chains";
+
+
+import {
+getNFT,
+
+balanceOf,
+
+totalSupply,
+
+} from "thirdweb/extensions/erc721";
+
+
+
+
+
+
+
 export async function POST(request: NextRequest) {
 
   const body = await request.json();
@@ -16,7 +42,7 @@ export async function POST(request: NextRequest) {
 
 
   // place 8 horses
-  // contractAddress = 0x41FBA0bd9f4DC9a968a10aEBb792af6A09969F60
+  const contractAddress = "0x41FBA0bd9f4DC9a968a10aEBb792af6A09969F60";
   // tokenId 0 to 9999
   // select 8 horses from 0 to 9999
 
@@ -24,14 +50,47 @@ export async function POST(request: NextRequest) {
   // 2. place 8 horses with the random numbers
   // non duplicate random numbers
 
+
+
+  const client = createThirdwebClient({
+    secretKey: process.env.THIRDWEB_SECRET_KEY || "",
+  });
+
+  if (!client) {
+    return NextResponse.json({
+      result: null,
+    });
+  }
+
+
+    const contractErc721 = getContract(
+      {
+        client: client,
+        chain: polygon,
+        address: contractAddress,
+      }
+    );
+
+
   const horses = [];
   const randomNumbers = [] as number[];
   while (randomNumbers.length < 8) {
     const randomNumber = Math.floor(Math.random() * 10000);
     if (!randomNumbers.includes(randomNumber)) {
+
       randomNumbers.push(randomNumber);
+
+
+      // getnft contractAddress and tokenId
+
+      const nft = await getNFT({
+        contract: contractErc721,
+        tokenId: BigInt(randomNumber),
+      });
+
       horses.push({
         tokenId: randomNumber,
+        nft: nft,
       });
     }
   }
