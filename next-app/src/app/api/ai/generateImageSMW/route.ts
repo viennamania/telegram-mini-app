a3aa654dc7ba7243b14307482b06714c125cf47d
seunghyonpark <creath.park@gmail.com>
2025-04-02 09:50:27 +0900

    Add random model selection for image generation in AI route

--- next-app/src/app/api/ai/generateImageSMW/route.ts
+++ next-app/src/app/api/ai/generateImageSMW/route.ts
@@ -92,8 +92,17 @@ export async function POST(request: NextRequest) {
     
     //model = "black-forest-labs/flux-dev";
 
-    model = "bytedance/sdxl-lightning-4step:5f24084160c9089501c1b3545d9be3c27883ae2239b6f412990e82d4a6210f8f";
-
+    const randomModel = Math.floor(Math.random() * 3);
+    if (randomModel == 0) {
+        hosting = "replicate";
+        model = "bytedance/sdxl-lightning-4step:5f24084160c9089501c1b3545d9be3c27883ae2239b6f412990e82d4a6210f8f";
+    } else if (randomModel == 1) {
+        hosting = "replicate";
+        model = "datacte/proteus-v0.2:06775cd262843edbde5abab958abdbb65a0a6b58ca301c9fd78fa55c775fc019";
+    } else if (randomModel == 2) {
+        hosting = "replicate";
+        model = "playgroundai/playground-v2.5-1024px-aesthetic:a45f82a1382bed5c7aeb861dac7c7d191b0fdf74d8d57c4a0e6ed7d4d0bf7d24";
+    }
 
 
     try {
