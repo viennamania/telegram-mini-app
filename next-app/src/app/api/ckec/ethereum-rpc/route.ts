a4375fbbc89695924cff34bdfcb21993d10bf64c
seunghyonpark <creath.park@gmail.com>
2025-04-23 10:17:23 +0900

    Refactor POST request handler to improve readability and maintainability

--- next-app/src/app/api/ckec/ethereum-rpc/route.ts
+++ next-app/src/app/api/ckec/ethereum-rpc/route.ts
@@ -1,31 +1,29 @@
-import { N } from "ethers";
 import { NextRequest, NextResponse } from "next/server";
+
+
+  /*
+  The CORS issue occurs because the server at https://shinemywinter.vercel.app/api/ckec/ethereum-rpc does not include the Access-Control-Allow-Origin header in its response.
+  */
+
+
 
 
 export async function POST(request: NextRequest) {
 
+
+
+  
   const body = await request.json();
 
-  /*
-  {"method":"eth_getBalance","params":["0x22571950F07e5acb92160E133B3878267c86aF56", "latest"],"id":1,"jsonrpc":"2.0"}
-  */
+  console.log("body", body);
 
 
-
-  const {
-    method,
-    params,
-    id,
-    jsonrpc,
-  } = body;
-
-
-  /*
-            curl http://52.78.186.199:8080 \
-          -X POST \
-          -H "Content-Type: application/json" \
-          --data '{"method":"eth_getBalance","params":["0x22571950F07e5acb92160E133B3878267c86aF56", "latest"],"id":1,"jsonrpc":"2.0"}'
-  */
+  
+       //     curl http://52.78.186.199:8080 \
+       //   -X POST \
+       //   -H "Content-Type: application/json" \
+       //   --data '{"method":"eth_getBalance","params":["0x22571950F07e5acb92160E133B3878267c86aF56", "latest"],"id":1,"jsonrpc":"2.0"}'
+  
   
 
   const url = "http://52.78.186.199:8080";
@@ -35,12 +33,7 @@ export async function POST(request: NextRequest) {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      method,
-      params,
-      id,
-      jsonrpc,
-    }),
+    body: body,
   })
     .then((res) => res.json())
     .catch((err) => {
@@ -51,11 +44,10 @@ export async function POST(request: NextRequest) {
       };
     });
 
-    
-
-
+  
   return NextResponse.json({
     result,
   });
+
   
 }
